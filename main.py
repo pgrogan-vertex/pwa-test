c30398a393fb458ebe82397d1ea83038382b87af
PatrickGrogan <patrickgrogan11@gmail.com>
2026-07-15 17:33:21 -0400

    switched from FastAPI to flask

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from flask import Flask, render_template
 
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = Flask(__name__)
 
 
 @app.get("/")
-async def root():
-    return FileResponse("templates/index.html")
+def root():
+    return render_template("index.html")
 
 
 @app.get("/api/hello")
-async def hello():
-    return {"message": "Hello from FastAPI! New string woahhh"}
+def hello():
+    return {"message": "Hello from Flask!"}
